dfc0cb7e4315e82ce53b9e346c333e979fab94d4
Alexander <stenberg.alexander@hotmail.com>
2022-12-06 22:37:38 +0100

    added some fetch routes

--- services/web/src/models/scooterModel.js
+++ services/web/src/models/scooterModel.js
@@ -1,8 +1,20 @@
 
 const scooterModel = {
 
-    fetchScooters: async function fetchScooters() {
-
+    fetchStockholmScooter: async function fetchStockholmScooter() {
+        const response = await fetch('https://localhost:3500/city/stockholm/Scooter')
+        const data = await response.json();
+        return data
+    },
+    fetchGoteborgScooter: async function fetchGoteborgScooter() {
+        const response = await fetch('https://localhost:3500/city/goteborg/Scooter')
+        const data = await response.json();
+        return data
+    },
+    fetchMalmoScooter: async function fetchMalmoScooter() {
+        const response = await fetch('https://localhost:3500/city/malmo/Scooter')
+        const data = await response.json();
+        return data
     }
 }
 
