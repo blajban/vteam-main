4a5b3e74b090dac0aa289486f6802c72f906889a
Alexander <stenberg.alexander@hotmail.com>
2022-12-06 21:59:03 +0100

    useState, useEffect

--- services/web/src/App.js
+++ services/web/src/App.js
@@ -1,6 +1,12 @@
+import React, { useState, useEffect } from 'react';
 import Map from './components/Map'
 import Navbar from './components/Navbar'
+
+
 function App() {
+  const [targetedItems, setTargetedItems] = useState(0);
+
+
   return (
     <div className="App">
       <Navbar></Navbar>
