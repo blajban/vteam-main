319b2cb37ea3536af1886ebbc4ca4635a2689556
Eric Samuelsson <gossen02@gmail.com>
2022-12-13 22:36:17 +0000

    Try to get token and user id to frontend

--- services/web/src/models/authModel.js
+++ services/web/src/models/authModel.js
@@ -1,13 +1,14 @@
 const authModel = {
 
     login: async function login() {
-        const response = await fetch('http://localhost:3500/login')
+        const response = await fetch('http://localhost:3500/login');
+        console.log(response);
         const data = await response.json();
         return data;
     },
 
     logout: async function logout() {
-        const response = await fetch('http://localhost:3500/logout')
+        const response = await fetch('http://localhost:3500/logout');
         const data = await response.json();
         return data;
     }
