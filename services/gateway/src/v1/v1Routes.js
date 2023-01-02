a7f0982ae13ba8c73dd6e7e8833175d12a4edf55
Eric Samuelsson <gossen02@gmail.com>
2023-01-02 20:39:43 +0000

    Comment drafts to new endpoints

--- services/gateway/src/v1/v1Routes.js
+++ services/gateway/src/v1/v1Routes.js
@@ -25,10 +25,10 @@ router.route('/city/:city/parking')
   .post(controllers.addParkingspot)
   .put(controllers.updateParkingspot)
   .delete(controllers.removeParkingspot)
-router.route('/users')
+router.route('/users/:loginId')
   .get(controllers.getUsers)
   .post(controllers.addUser);
-router.route('/users/:userId')
+router.route('/users/:loginId/:userId')
   .get(controllers.getUsers)
   .put(controllers.updateUser)
   .delete(controllers.removeUser);
@@ -36,7 +36,7 @@ router.route('/login')
   .get(controllers.login);
 router.route('/getToken/:code')
   .get(controllers.getToken);
-router.route('/getGitHubUser/:token')
+router.route('/getGitHubUser')
   .get(controllers.getGitHubUser);
 router.route('/invoice')
   .post(controllers.addInvoice);
@@ -49,5 +49,56 @@ router.route('/rates')
   .post(controllers.addRate)
   .put(controllers.updateRate)
   .delete(controllers.removeRate);
+
+/*
+router.route('/eventflows/rent_scooter/:scooterId/:userId')
+  .get(controllers.rentScooter);
+router.route('/eventflows/park_scooter/:scooterId')
+  .get(controllers.parkScooter);
+router.route('/eventflows/simulate_scooters')
+  .get(controllers.simulateScooters); // Todo
+router.route('/eventflows/stop_simulation')
+  .get(controllers.stopSimulation); // Todo
+router.route('/eventflows/addRandomScooters/:city/:number')
+  .get(controllers.addRandomScooters);
+router.route('/city/:city/scooter')
+  .get(controllers.getScooters)
+  .post(controllers.addScooter);
+router.route('/city/:city/scooter/:scooterId')
+  .get(controllers.getScooters)
+  .put(controllers.updateScooter)
+  .delete(controllers.removeScooter);
+router.route('/city/:city/parking')
+  .get(controllers.getParkingspots)
+router.route('/city/:city/parking/:loginId')
+  .post(controllers.addParkingspot)
+  .put(controllers.updateParkingspot)
+  .delete(controllers.removeParkingspot)
+router.route('/users/:loginId')
+  .get(controllers.getUsers)
+  .post(controllers.addUser);
+router.route('/users/:loginId/:userId')
+  .get(controllers.getUsers)
+  .put(controllers.updateUser)
+  .delete(controllers.removeUser);
+router.route('/login')
+  .get(controllers.login);
+router.route('/getToken/:code')
+  .get(controllers.getToken);
+router.route('/getGitHubUser')
+  .get(controllers.getGitHubUser);
+router.route('/invoice/:loginId')
+  .post(controllers.addInvoice);
+router.route('/invoice/user/:userId/:loginId')
+  .get(controllers.getInvoices);
+router.route('/invoice/:invoiceId/:loginId')
+  .get(controllers.getInvoices);
+router.route('/rates')
+  .get(controllers.getRates)
+router.route('/rates/:loginId')
+  .post(controllers.addRate)
+  .put(controllers.updateRate)
+  .delete(controllers.removeRate);
+*/
 
 module.exports = router;
