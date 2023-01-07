80f978b8c6cd09a9c351a072ef621d66b97362df
Eric Samuelsson <gossen02@gmail.com>
2023-01-07 18:14:35 +0000

    Hides content if the user is not logged in

--- services/web/src/App.js
+++ services/web/src/App.js
@@ -12,6 +12,7 @@ function App() {
   const [token, setToken] = useState("");
   const [userId, setUserId] = useState("");
   const [code, setCode] = useState("");
+  const [admin, setAdmin] = useState(false);
 
   function login() {
     window.location.href = 'http://localhost:3500/login';
@@ -21,17 +22,9 @@ function App() {
     window.location.reload();
   }
 
-  async function testFunc() {
-    const userInfo = await usersModel.getUser(userId, token);
-    console.log("Userid:", userInfo._id);
-    console.log("Name:", userInfo.name);
-    console.log("Mail:", userInfo.mail);
-    console.log("Mobile:", userInfo.mobile);
-    console.log("Zip:", userInfo.zip);
-    console.log("Address:", userInfo.address);
-    console.log("City:", userInfo.city);
-    console.log("Admin:", userInfo.admin);
-    console.log("Balance:", userInfo.balance);
+  async function checkAdmin() {
+    const userInfo = await usersModel.getUser(token, userId, userId);
+    setAdmin(userInfo.admin);
   }
 
   useEffect(() => {
@@ -65,29 +58,41 @@ function App() {
   useEffect(() => {
     (async () => {
       if (userId) {
-        await testFunc();
+        await checkAdmin();
       }
     })();
   }, [userId]);
 
   return (
-    /** <Route path="/Userprofile" element={<UserProfile token={token} userId={userId} />}></Route>*/
     <div className="App">
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/Map" element={<Mapside/>}></Route>
-        <Route path="/Userprofile" element={<UserProfile/>}></Route>
-        <Route path="/Admincontroll" element={<AdminControll/>}></Route>
-      </Routes>
-      {token ?
+      {token && userId && admin ?
         <>
-          <p>You are logged in</p>
+          <Navbar admin={admin}></Navbar>
+          <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/Map" element={<Mapside token={token} userId={userId} />}></Route>
+            <Route path="/Admincontroll" element={<AdminControll token={token} userId={userId} admin={admin} />}></Route>
+            <Route path="/Userprofile" element={<UserProfile token={token} userId={userId} admin={admin} />}></Route>
+          </Routes>
           <button onClick={logout}>Logout</button>
         </>
         :
         <>
-          <button onClick={login}>Login</button>
+        {token && userId ?
+          <>
+            <Navbar admin={admin}></Navbar>
+            <Routes>
+              <Route path="/" element={<Home/>}></Route>
+              <Route path="/Map" element={<Mapside token={token} userId={userId} />}></Route>
+              <Route path="/Userprofile" element={<UserProfile token={token} userId={userId} admin={admin} />}></Route>
+            </Routes>
+            <button onClick={logout}>Logout</button>
+          </>
+          :
+          <>
+            <button onClick={login}>Login</button>
+          </>
+        }
         </>
       }
     </div>
