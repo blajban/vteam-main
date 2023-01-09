f8526cc7755140d382832be9e52c8f89b3bab15c
Eric Samuelsson <gossen02@gmail.com>
2023-01-09 18:14:25 +0000

    Add userprofile

--- services/web/src/components/Userprofile.js
+++ services/web/src/components/Userprofile.js
@@ -1,14 +1,118 @@
+import { useEffect, useState } from 'react';
+import usersHandler from '../models/usersModel';
 
+function Userprofile({token, userId}) {
+    const [user, setUser] = useState([]);
+    const [name, setName] = useState("");
+    const [mail, setMail] = useState("");
+    const [mobile, setMobile] = useState("");
+    const [address, setAddress] = useState("");
+    const [zip, setZip] = useState("");
+    const [city, setCity] = useState("");
+    const [balance, setBalance] = useState("");
 
+    async function getUserInfo() {
+        const result = await usersHandler.getUser(token, userId, userId);
+        setUser(result);
+    }
 
-function Userprofile({userId}) {
+    async function updateUser(e) {
+        e.preventDefault();
+        const userToUpdate = {
+            _id: userId,
+            name: name,
+            mail: mail,
+            mobile: mobile,
+            address: address,
+            zip: zip,
+            city: city,
+            admin: user.admin,
+            balance: balance
+        }
+        await usersHandler.updateUser(token, userToUpdate, userId);
+        await getUserInfo();
+    }
+
+    useEffect(() => {
+        (async () => {
+            await getUserInfo();
+        })();
+    }, []);
+
     return (
         <div className="user-profile-container">
             <div className="image-holder">
-            <img src={require('../assets/user.png')}></img>
+                <img src={require('../assets/user.png')}></img>
             </div>
-            <h3>Alexander@email.com</h3>
+
+            <form>
+                <label>Name
+                    <br></br>
+                    <input
+                        type="text"
+                        defaultValue={user.name}
+                        onChange={(event) => setName(event.target.value)}
+                    />
+                </label>
+                <br></br>
+                <label>Email
+                    <br></br>
+                    <input
+                        type="email"
+                        defaultValue={user.email}
+                        onChange={(event) => setMail(event.target.value)}
+                    />
+                </label>
+                <br></br>
+                <label>Phone number
+                    <br></br>
+                    <input
+                        type="tel"
+                        defaultValue={user.mobile}
+                        onChange={(event) => setMobile(event.target.value)}
+                    />
+                </label>
+                <br></br>
+                <label>Address
+                    <br></br>
+                    <input
+                        type="text"
+                        defaultValue={user.address}
+                        onChange={(event) => setAddress(event.target.value)}
+                    />
+                </label>
+                <br></br>
+                <label>Zip code
+                    <br></br>
+                    <input
+                        type="text"
+                        defaultValue={user.zip}
+                        onChange={(event) => setZip(event.target.value)}
+                    />
+                </label>
+                <br></br>
+                <label>City
+                    <br></br>
+                    <input
+                        type="text"
+                        defaultValue={user.city}
+                        onChange={(event) => setCity(event.target.value)}
+                    />
+                </label>
+                <br></br>
+                <label>Balance
+                    <br></br>
+                    <input
+                        type="number"
+                        step="0.01"
+                        defaultValue={user.balance}
+                        onChange={(event) => setBalance(event.target.value)}
+                    />
+                </label>
+                <br></br>
+                <button onClick={(e) => updateUser(e)}>Uppdatera uppgifter</button>
+            </form>
         </div>
-        );
+        )
     }
 export default Userprofile;
